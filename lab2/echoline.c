96434484da6e5894119164c5bc5bd1da3f03ff2d
yurtse1 <yurtse1@stolaf.edu>
2018-02-26 16:51:03 -0600

    Step 6 complete: echoline.c

--- lab2/echoline.c
+++ lab2/echoline.c
@@ -8,23 +8,34 @@ struct name {
   int status;
 };
 
+//enum status_value { NORMAL, EOF_OR_ERROR };
+
 int read_name(struct name* input) {
-  
+
   input -> tok = malloc(MAXTOKS sizeof(char *));
   char *line; 
   size_t size;
   size_t chars;
+
+
     chars = getline(&line, &size, stdin);
     input -> tok[0] = line;
 
-    if (chars == -1) {
-      printf("There is no input.\n");
-    }
-    else {
+
+  enum status_value { NORMAL, EOF_OR_ERROR };
+  if (chars == -1) {
+    input -> status = EOF_OR_ERROR;
+  }
+  else {
+    input -> status = NORMAL;
+  //  input -> status = status_value;
       printf("%zu characters were read.\n", chars-1); 
       printf("you typed : %s \n", input->tok[0]);    
-    }
-    return 0;
+  }
+  if (input -> status = NORMAL) 
+      return 1;
+    else
+      return 0;
 }
 
 
@@ -32,6 +43,19 @@ int main()
 {
   struct name buffer;
   read_name(&buffer);
+  //  buffer -> tok = malloc(MAXTOKS*sizeof(char));
 
+    /*  printf("Type something: ");
+    //  line = (char *)malloc(size * sizeof(char));
+    chars = getline(&line, &size, stdin);
+
+    if (chars == -1) {
+      printf("There is no input.\n");
+    }
+    else {
+      printf("%zu characters were read.\n", chars-1); 
+      printf("you typed : %s \n", line);    
+    }
+    return 0;*/
 }
 
