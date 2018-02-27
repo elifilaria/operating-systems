d10ec845cba90cfbf8132df7b0dcb0037e51534a
yurtse1 <yurtse1@stolaf.edu>
2018-02-26 18:19:08 -0600

    Step 7 complete: echoline.c

--- lab2/echoline.c
+++ lab2/echoline.c
@@ -2,40 +2,54 @@
 #include <stdlib.h>
 #define MAXTOKS
 
+
 struct name {
   char **tok;
   int count;
   int status;
 };
 
-//enum status_value { NORMAL, EOF_OR_ERROR };
 
 int read_name(struct name* input) {
-
-  input -> tok = malloc(MAXTOKS sizeof(char *));
+  
+  int N = 0;
+  // input -> tok = malloc((N+1)* sizeof(char**));
+  input -> tok = malloc(MAXTOKS sizeof(char **));
   char *line; 
   size_t size;
   size_t chars;
-
-
-    chars = getline(&line, &size, stdin);
-    input -> tok[0] = line;
-
-
+  
+  // int N = 0;
+  //  int i = 0;
+  chars = getline(&line, &size, stdin);
+  //  input -> tok[0] = malloc((N+1)* sizeof(char));
+  while(!isspace(line[N])){
+    N++;
+    input -> tok[0] = malloc((N+1)* sizeof(char));
+  }  
+ 
+  int i = 0;
+  while(i < N+1){
+    input -> tok[0] = &line[i]; 
+    i++;
+    input -> tok[N+1] == NULL;
+  }
+  // input -> tok[N+1] == NULL;
+ 
+  
   enum status_value { NORMAL, EOF_OR_ERROR };
   if (chars == -1) {
     input -> status = EOF_OR_ERROR;
   }
   else {
     input -> status = NORMAL;
-  //  input -> status = status_value;
-      printf("%zu characters were read.\n", chars-1); 
-      printf("you typed : %s \n", input->tok[0]);    
+    printf("%zu characters were read.\n", chars-1); 
+    printf("you typed : %s \n", line);    
   }
   if (input -> status = NORMAL) 
-      return 1;
-    else
-      return 0;
+    return 1;
+  else
+    return 0;
 }
 
 
@@ -43,19 +57,4 @@ int main()
 {
   struct name buffer;
   read_name(&buffer);
-  //  buffer -> tok = malloc(MAXTOKS*sizeof(char));
-
-    /*  printf("Type something: ");
-    //  line = (char *)malloc(size * sizeof(char));
-    chars = getline(&line, &size, stdin);
-
-    if (chars == -1) {
-      printf("There is no input.\n");
-    }
-    else {
-      printf("%zu characters were read.\n", chars-1); 
-      printf("you typed : %s \n", line);    
-    }
-    return 0;*/
 }
-
